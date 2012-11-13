f23b5ae818bf05a80736f51f57fd3867d2eeb960
Zidni Mubarock <zidmubarock@gmail.com>
2012-11-13 23:40:30 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Docoll::Application.config.secret_token = 'dc9d87641a32cd5cd697d8438fbc29dc63afa7fee596c6eb0d117de5b5336fcd0ec6264f09a77d8bb381e4ff66507769ede2cac3eae5a7d14d392512315cb20f'
